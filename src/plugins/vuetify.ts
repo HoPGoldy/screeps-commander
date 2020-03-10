f8ee44aac38b3d32cb84bd21c47a9bb5a3956bd3
huang puguang <puguang_huang@fpi-inc.com>
2020-03-10 15:49:55 +0800

    feat: 引入官方图标库

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import '@mdi/font/css/materialdesignicons.css'
 import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
 import VuetifyToast from 'vuetify-toast-snackbar'
 
